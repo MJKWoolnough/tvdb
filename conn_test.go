4824dfbad93da444895f7d0093d58373e9a9bb82
Michael Woolnough <michael.woolnough@gmail.com>
2016-10-05 12:07:22 +0100

    added simple refresh test

--- conn_test.go
+++ conn_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestLogin(t *testing.T) {
+	t.Parallel()
 	_, err := tvdb.Login(auth)
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
@@ -14,5 +15,16 @@ func TestLogin(t *testing.T) {
 	_, err = tvdb.Login(tvdb.Auth{})
 	if err != tvdb.ErrInvalidAuth {
 		t.Errorf("expecting invalid auth, got %s", err)
+	}
+}
+
+func TestRefresh(t *testing.T) {
+	t.Parallel()
+	c, err := tvdb.Login(auth)
+	if err != nil {
+		t.Errorf("unexpected login error: %s", err)
+	}
+	if err := c.Refresh(); err != nil {
+		t.Errorf("unexpected refresh error: %s", err)
 	}
 }
